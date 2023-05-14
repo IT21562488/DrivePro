23af03379288d427322f3737f46339a80aff293f
IT21562488 <it21562488@my.sliit.lk>
2023-05-14 13:48:17 +0530

    drivePro-it21192296

--- app/src/main/java/com/example/signupandsignin/activities/Signup_driver.kt
+++ app/src/main/java/com/example/signupandsignin/activities/Signup_driver.kt
@@ -57,6 +57,7 @@ class signup_driver : AppCompatActivity() {
 
                 }
 
+
             } else {
                 if (Email.isEmpty()){
                     Toast.makeText(this,"Please fill Email", Toast.LENGTH_LONG).show()
